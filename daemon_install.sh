fd938ae6f8e2ae41775f8c5ce4a420b7b6430a39
astavonin <alex@sysdev.me>
2013-02-23 20:37:09 +0900

    Installation fix
    installer can not install daemon as PackageMaker doesn't allow set up
    root ownership from UI

--- daemon_install.sh
+++ daemon_install.sh
@@ -3,11 +3,15 @@
 WATCH_DIR="/Library/Application Support/TasksExplorer/watch_dir"
 OLD_PLIST="/Library/LaunchDaemons/com.stavonin.tasksexplorerd.plist"
 NEW_PLIST="/Library/LaunchDaemons/com.macosinternals.tasksexplorerd.plist"
+DAEMON="/Library/Application Support/TasksExplorer/tasksexplorerd"
 
 if [[ ! -d "$WATCH_DIR" ]]; then
 	sudo mkdir "$WATCH_DIR"
 	sudo chmod uga+wr "$WATCH_DIR"
 fi
+
+sudo chown root:staff $NEW_PLIST
+sudo chown root:staff $DAEMON
 
 if [[ -e $OLD_PLIST ]]; then
 	sudo launchctl unload "$OLD_PLIST"
