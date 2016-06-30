a5941ec2c18295f2aa21ca8e2d410f7ebdfb0293
Alexander Stavonin <alex@sysdev.me>
2016-06-30 20:22:34 +0800

    Tasks information extraction in async mode

--- tasksexplorerd/task/tasks_monitor_impl.cpp
+++ tasksexplorerd/task/tasks_monitor_impl.cpp
@@ -1,9 +1,11 @@
 #include "tasks_monitor_impl.h"
 #include <assert.h>
 #include <mach/host_priv.h>
-#include "task_impl.h"
+#include <list>
 #include "errors.h"
 #include "system_helpers.h"
+#include "task_impl.h"
+#include "utils.h"
 
 namespace tasks
 {
@@ -35,20 +37,29 @@ tasks_monitor_impl::tasks_map_ptr tasks_monitor_impl::active_tasks()
 
     m_stamp++;
 
+    std::list<std::future<std::shared_ptr<task_impl>>> tasks_list;
     for( auto &proc : procs )
     {
         auto pid  = proc.kp_proc.p_pid;
         auto task = m_tasks->find( pid );
         if( task == m_tasks->end() )
         {
-            m_tasks->emplace( std::make_pair(
-                pid, std::make_shared<task_impl>( m_stamp, proc, m_log ) ) );
+            auto f = utils::async( [&proc, this ]() -> auto {
+                return std::make_shared<task_impl>( m_stamp, proc, m_log );
+            } );
+            tasks_list.emplace_back( std::move( f ) );
         }
         else
         {
             auto task_i = std::static_pointer_cast<task_impl>( task->second );
             task_i->refresh( m_stamp, proc );
         }
+    }
+
+    for( auto &tf : tasks_list )
+    {
+        auto t = tf.get();
+        m_tasks->insert( std::make_pair( t->pid(), t ) );
     }
 
     for( auto it = m_tasks->begin(); it != m_tasks->end(); )
--- tasksexplorerd/utils/utils.h
+++ tasksexplorerd/utils/utils.h
@@ -1,9 +1,32 @@
 #pragma once
 
 #include <string>
+#include <future>
+#include <dispatch/dispatch.h>
 
 namespace utils
 {
 std::string exec_dir();
-void hex_dump( const char *desc, const void *addr, int len );
+void hex_dump( const char* desc, const void* addr, int len );
+
+template <class Function, class... Args>
+std::future<std::result_of_t<std::decay_t<Function>( std::decay_t<Args>... )>>
+async( Function&& f, Args&&... args )
+{
+    using result_t   = typename std::result_of<Function( Args... )>::type;
+    using packaged_t = typename std::packaged_task<result_t()>;
+
+    auto task = new packaged_t( std::bind( std::forward<Function>( f ),
+                                           std::forward<Args>( args )... ) );
+    auto res = task->get_future();
+    dispatch_async_f(
+        dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 ), task,
+        []( void* task_ptr ) {
+            auto smart_task_ptr = std::unique_ptr<packaged_t>(
+                static_cast<packaged_t*>( task_ptr ) );
+
+            ( *smart_task_ptr )();
+        } );
+    return res;
+}
 }
